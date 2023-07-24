d888b3eba01a48ccdf3cb8b62f1b69e4a73b6857
afihas <hassnae.afilal@gmail.com>
2023-07-24 07:54:16 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
  * puts2 - Prints every other character of a string
  * @str: The string to be retreated
  * Return: void
  */
-
 void puts2(char *str)
-
 {
-	
 	int i;
 	int j = 0;
 
